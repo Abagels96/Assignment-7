b7abb1689468860da58a0e14e53bbaba651e34cd
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2025-01-15 19:08:28 -0600

    fix some stuff

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -18,8 +18,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	}
 
-	{
-	}
+	
 
 	@Override
 	public int getSize() {
@@ -31,7 +30,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 	public T get(int index) throws IndexOutOfBoundsException {
 
 		int i = index;
-		if (index >= 0 && index <= size - 1) {
+		if (index >= 0 && index < size) {
 
 			return (T) items[i];
 
@@ -51,20 +50,26 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+//items[index] = item; when index == size: This will fail if the index is equal to size because the array might not be resized, and you would try to access an out-of-bounds index. You need to handle the case of adding to the next available slot.
+//
+//The general structure of this method is incorrect. Let's break it down:
+//
 
-		if (index >= 0 && index < size) {
-				items = shiftRight(items, index);
-				items[index] = item;
-				size++;
-			} else if(index==size) {
-				items= isArrayFull(items);
-				items[index] = item;
-				size++;
-				System.out.println(Arrays.toString(items));
-			}
-			else {
-		throw new IndexOutOfBoundsException();
-			}
+if(index<0|| index>size) {
+	throw new IndexOutOfBoundsException();
+	
+}
+	 if(size==index) {
+		items= isArrayFull(items);
+	}
+	if(index<size) {
+		items=shiftRight(items, index);
+	}
+			
+			items[index]=item;
+			size++;
+			
+			
 			return true;
 		
 
@@ -72,22 +77,23 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	private Object[] shiftRight(Object[] items, int index) {
  if(index<size) {
-		System.arraycopy(items, index, items, index + 1, size - index - 1);}
+		System.arraycopy(items, index, items, index + 1, size - index );}
 
 		return items;
  
 	}
+	
 
 	
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
 		Object removed = items[index];
-		if (index >= 0 && index <= size - 1) {
- 			System.arraycopy(items, index + 1, items, index, items.length - index - 1);
-			Arrays.toString(items);
+		if (index >= 0 && index < size) {
+ 			System.arraycopy(items, index + 1, items, index, size - index - 1);
 			size--;
 
-		} else if (index > size - 1) {
+		}
+		else if (index > size - 1) {
 			throw new IndexOutOfBoundsException();
 		}
 
